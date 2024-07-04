35f52c77c3fa35b7e82c6bec7435e23a2611e681
Marly Valcin <marlyvalcin@gmail.com>
2024-07-03 20:17:10 -0400

    updated the delete request for product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -92,8 +92,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if(!deletedProduct) {
+      res.status(400).json({message: 'No product was found with that id'})
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
 });
 
 module.exports = router;
