98e629fee7013ea49c93e5c607b44c9805b1b455
Marly Valcin <marlyvalcin@gmail.com>
2024-07-03 20:06:01 -0400

    updated the put method and the get method to find one category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
     const findOneCategory = await Category.findOne({
-      where: {category: req.body.category},
+      where: {category: req.params.id},
       include: [{ model: Product }]
     })
     res.status(200).json(findOneCategory);
@@ -33,9 +33,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const newCategory = await Category.create({
-    category_name: req.body
-    })
+    const newCategory = await Category.create(req.body)
     res.status(200).json(newCategory);
   } catch (error) {
     res.status(400).json(error);
@@ -43,9 +41,18 @@ router.post('/', async (req, res) => {
   }  
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-
+  try {
+    const updateCategory = await Category.update( req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+  } catch (error) {
+    res.status(400).json(error);
+    console.error(error.message);
+  }
 });
 
 router.delete('/:id', (req, res) => {
